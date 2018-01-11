3cf2ae010b45402be1b1623d6a5d1e73105cf004
Tiago Temporin <tiaguinhocab@gmail.com>
2018-01-11 16:58:52 +0000

    feat(selector) add reactive forms and input data from forms

--- lib/src/color-picker-selector.component.ts
+++ lib/src/color-picker-selector.component.ts
@@ -12,6 +12,7 @@ import {
     Renderer2,
     ViewChild
 } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
@@ -30,17 +31,31 @@ interface ColorOption {
 })
 export class MatColorPickerSelectorComponent implements AfterViewInit, OnInit, OnDestroy {
 
+    /**
+     * ElemenRef of the main color
+     */
     @ViewChild('block') _block: ElementRef;
+    // hold _block context
+    private _blockContext: any;
 
+    /**
+     * ElementRef of the color base
+     */
     @ViewChild('strip') _strip: ElementRef;
+    // hold _strip context
+    private _stripContext: any;
 
+    /**
+     * Receive selected color from the component
+     */
     @Input()
     get selectedColor(): string { return this._selectedColor; }
     set selectedColor(value: string) {
         this._selectedColor = value || '';
 
         if (!this._isPressed) {
-            this._rgbaColor = this._getRgba();
+            this._updateRGB();
+            this._updateRGBA();
             if (this._blockContext) {
                 this._fillGradient();
             }
@@ -48,37 +63,80 @@ export class MatColorPickerSelectorComponent implements AfterViewInit, OnInit, O
     }
     private _selectedColor: string = '';
 
+    /**
+     * Emit update when a color is selected
+     */
     @Output() changeSelectedColor = new EventEmitter;
 
-    private _blockContext: any;
-
-    private _stripContext: any;
-
+    /**
+     * RGBA current color
+     */
     private _rgbaColor: string = 'rgba(255,0,0,1)';
 
+    /**
+     * Subject of the current selected color by the user
+     */
     private _tmpSelectedColor: BehaviorSubject<string>;
 
+    /**
+     * Subscription of the tmpSelectedColor Observable
+     */
     private _tmpSelectedColorSub: Subscription;
 
+    /**
+     * Validate if the mouse button is pressed
+     */
     _isPressed: boolean = false;
 
-    rgb: ColorOption[] = [
-        { type: 'R', value: 0},
-        { type: 'G', value: 0},
-        { type: 'B', value: 0}
-    ];
+    /**
+     * Form of the color in hexa
+     */
+    hexForm: FormGroup;
 
-    constructor(private render: Renderer2) {}
+    /**
+     * Form and keys of the fields in RGB
+     */
+    rgbKeys = ['R', 'G', 'B'];
+    rgbForm: FormGroup;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private render: Renderer2
+    ) {}
 
     ngOnInit() {
         this._tmpSelectedColor = new BehaviorSubject<string>(this._selectedColor);
         this._tmpSelectedColorSub = this._tmpSelectedColor.subscribe(color => {
             if (color !== this._selectedColor) {
+                this.hexForm.setValue({hexCode: color});
                 this.changeSelectedColor.emit(color);
             }
         });
+
+        // hex form
+        this.hexForm = this.formBuilder.group({
+            hexCode: [this.selectedColor, [Validators.minLength(7), Validators.maxLength(7)]]
+        });
+
+        // rgb dynamic form
+        const rgbGroup: any = {};
+        const rgbValue: number[] = this._getRGB();
+        this.rgbKeys.forEach((key, index) => rgbGroup[key] =
+            new FormControl(rgbValue[index], [
+                Validators.min(0),
+                Validators.max(256),
+                Validators.minLength(1),
+                Validators.maxLength(3)
+            ]));
+        this.rgbForm = this.formBuilder.group(rgbGroup);
+
+        // watch changes on forms
+        this._onChanges();
     }
 
+    /**
+     * Destroy all subscriptions
+     */
     ngOnDestroy() {
         if (this._tmpSelectedColorSub && !this._tmpSelectedColorSub.closed) {
             this._tmpSelectedColorSub.unsubscribe();
@@ -119,7 +177,7 @@ export class MatColorPickerSelectorComponent implements AfterViewInit, OnInit, O
     }
 
     /**
-     *
+     * Generate colors based on the RGBA color
      */
     private _fillGradient(): void {
         this._blockContext.fillStyle = this._rgbaColor;
@@ -147,15 +205,44 @@ export class MatColorPickerSelectorComponent implements AfterViewInit, OnInit, O
     }
 
     /**
-     * 
+     * Watch change on forms
      */
-    private _getRgba(data?: any): string {
-        if (!this._selectedColor && !data) {
-            return 'rgba(255,0,0,1)';
-        }
+    private _onChanges() {
+        // validate digited code and update when digitation is finished
+        this.hexForm.get('hexCode').valueChanges.subscribe(value => {
+            if (!this._isPressed) {
+                if (value.length === 7) {
+                    this.selectedColor = value;
+                }
+            }
+        });
 
+        this.rgbForm.valueChanges.subscribe(controls => {
+            const data: number[] = [];
+            for (const key in controls) {
+                if (!controls[key] || controls[key] > 255) {
+                    data.push(0);
+                    continue;
+                }
+
+                data.push(controls[key]);
+            }
+
+            const hex = this._getHex(data);
+            if (hex !== this._selectedColor && hex.length === 7) {
+                this._tmpSelectedColor.next(hex);
+            }
+        });
+    }
+
+    /**
+     * Convert HEX/canvas value to rgb
+     * @param data any
+     * @returns number[]
+     */
+    private _getRGB(data?: any): number[] {
         if (data) {
-            return `rgba(${data[0]}, ${data[1]}, ${data[2]}, 1)`;
+            return [data[0], data[1], data[2]];
         }
 
         const hex = this._selectedColor.replace('#', '');
@@ -163,33 +250,62 @@ export class MatColorPickerSelectorComponent implements AfterViewInit, OnInit, O
         const g = parseInt(hex.slice(2, 4), 16);
         const b = parseInt(hex.slice(4, 6), 16);
 
-        this.rgb = [
-            { type: 'R', value: r},
-            { type: 'G', value: g},
-            { type: 'B', value: b}
-        ];
-
-        return `rgba(${r}, ${g}, ${b}, 1)`;
-    }
-
-    private _getHex(data: any): string {
-        return `#${data[0].toString(16)}${data[1].toString(16)}${data[2].toString(16)}`;
+        return [r, g, b];
     }
 
     /**
-     *
+     * Convert RGB value to HEX
+     * @param data any
+     * @returns string
+     */
+    private _getHex(data: any): string {
+        return `#${data[0].toString(16)}${data[1].toString(16)}${data[2].toString(16)}`
+                .toUpperCase();
+    }
+
+    /**
+     * Update RGBA color
+     * @param data any
+     */
+    private _updateRGBA(data?: any): void {
+        if (!this._selectedColor && !data) {
+            this._rgbaColor = 'rgba(255,0,0,1)';
+        }
+
+        const rgb = this._getRGB(data);
+        this._rgbaColor = `rgba(${rgb[0]}, ${rgb[1]}, ${rgb[2]}, 1)`;
+    }
+
+    /**
+     * Update RGB form
+     * @param data any
+     */
+    private _updateRGB(data?: any): void {
+        if (!this.rgbForm) { return; }
+
+        if (!data) {
+            data = this._getRGB();
+        }
+
+        this.rgbForm.setValue({'R': data[0], 'G': data[1], 'B': data[2]});
+    }
+
+    /**
+     * Get selected base color from the canvas
+     * @param e MouseEvent
      */
     private changeBaseColor(e): void {
         if (this._isPressed) {
             const data = this._stripContext.getImageData(e.offsetX, e.offsetY, 1, 1).data;
-            this._rgbaColor = this._getRgba(data);
+            this._updateRGBA(data);
             this._fillGradient();
             this.updateValues(data);
         }
     }
 
     /**
-     *
+     * Get selected color from the canvas
+     * @param e MouseEvent
      */
     private changeColor(e): void {
         if (this._isPressed) {
@@ -198,13 +314,12 @@ export class MatColorPickerSelectorComponent implements AfterViewInit, OnInit, O
         }
     }
 
+    /**
+     * Emit update from the selected color
+     * @param data any
+     */
     private updateValues(data: any): void {
         if (data) {
-            this.rgb = [
-                { type: 'R', value: data[0]},
-                { type: 'G', value: data[1]},
-                { type: 'B', value: data[2]}
-            ];
             this._tmpSelectedColor.next(this._getHex(data));
         }
     }
