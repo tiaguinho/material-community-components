ad445f036167efa94d8db192576ec6d7ecbbbd4e
Tiago Temporin <tiaguinhocab@gmail.com>
2018-01-11 14:42:52 +0000

    docs(picker) improve documentation

--- lib/src/color-picker.component.ts
+++ lib/src/color-picker.component.ts
@@ -28,59 +28,97 @@ import { Observable } from 'rxjs/Observable';
 })
 export class MatColorPickerComponent implements AfterContentInit, OnInit, OnDestroy {
 
+    /**
+     * Get all collections
+     */
     @ContentChildren(MatColorPickerCollectionComponent)
     _collections: QueryList<MatColorPickerCollectionComponent>;
 
+    /**
+     * Change label of the collection UsedColors
+     */
     @Input()
     get usedColorLabel(): string { return this._usedColorLabel; }
     set usedColorLabel(value: string) { this._usedColorLabel = value; }
     private _usedColorLabel: string = 'Used Colors';
 
+    /**
+     * Hide empty slots from the collection UsedColors
+     */
     @Input('hideEmptyUsedColors')
     get hideEmpty(): boolean { return this._hideEmpty; }
     set hideEmpty(value: boolean) { this._hideEmpty = coerceBooleanProperty(value); }
     private _hideEmpty: boolean = false;
 
+    /**
+     * Start with a color selected
+     */
     @Input()
     get selectedColor(): string { return this._selectedColor; }
     set selectedColor(value: string) { this._selectedColor = value; }
     private _selectedColor: string;
 
+    /**
+     * Define if the panel will be initiated open
+     */
     @Input()
     get isOpen(): boolean { return this._isOpen; }
     set isOpen(value: boolean) { this._isOpen = coerceBooleanProperty(value); }
     private _isOpen: boolean = false;
 
+    /**
+     * Hide the action buttons (cancel/confirm)
+     */
     @Input()
     get hideButtons(): boolean { return this._hideButtons; }
     set hideButtons(value: boolean) { this._hideButtons = coerceBooleanProperty(value); }
     private _hideButtons: boolean = false;
 
+    /**
+     * Change btnCancel label
+     */
     @Input() btnCancel: string = 'Cancel';
 
+    /**
+     * Change btnConfirm label
+     */
     @Input() btnConfirm: string = 'Confirm';
 
+    /**
+     * Emit changes when panel is closed
+     */
     @Output() change = new EventEmitter;
 
+    /**
+     * Return a Observable with the color the user is picking
+     */
     get tmpSelectedColor$(): Observable<string> { return this._tmpSelectedColor.asObservable(); }
     private _tmpSelectedColor: BehaviorSubject<string>;
 
+    /**
+     * Array of subscriptions from the collections
+     */
     private _collectionSubs: Subscription[] = [];
 
+    /**
+     * Observable with all the colors used by the user
+     */
     usedColors$: Observable<string[]>;
 
     constructor(
         private elementRef: ElementRef,
         private changeDetectorRef: ChangeDetectorRef,
         private colorPickerService: MatColorPickerService
-    ) {
-        this.usedColors$ = colorPickerService.getColors();
-    }
+    ) {}
 
     ngOnInit() {
+        this.usedColors$ = this.colorPickerService.getColors();
         this._tmpSelectedColor = new BehaviorSubject<string>(this._selectedColor);
     }
 
+    /**
+     * Walk throw all collections and subcribe to changes
+     */
     ngAfterContentInit() {
         if (this._collections) {
             this._collections.forEach((collection: MatColorPickerCollectionComponent) => {
@@ -93,6 +131,9 @@ export class MatColorPickerComponent implements AfterContentInit, OnInit, OnDest
         }
     }
 
+    /**
+     * Destroy all subscriptions
+     */
     ngOnDestroy() {
         if (this._collectionSubs) {
             this._collectionSubs.forEach((subscription: Subscription) => {
@@ -103,6 +144,9 @@ export class MatColorPickerComponent implements AfterContentInit, OnInit, OnDest
         }
     }
 
+    /**
+     * Update selected color and emit the change
+     */
     private _updateSelectedColor() {
         if (this._isOpen) {
             this._selectedColor = this._tmpSelectedColor.getValue();
@@ -110,6 +154,9 @@ export class MatColorPickerComponent implements AfterContentInit, OnInit, OnDest
         }
     }
 
+    /**
+     * Open/close color picker panel
+     */
     toggle() {
         this._isOpen = !this._isOpen;
         if (!this._isOpen) {
@@ -117,6 +164,10 @@ export class MatColorPickerComponent implements AfterContentInit, OnInit, OnDest
         }
     }
 
+    /**
+     * Update tmpSelectedColor
+     * @param color string
+     */
     updateTmpSelectedColor(color: string) {
         if (color) {
             this._tmpSelectedColor.next(color);
@@ -126,7 +177,9 @@ export class MatColorPickerComponent implements AfterContentInit, OnInit, OnDest
         }
     }
 
-
+    /**
+     * Update selectedColor and close the panel
+     */
     confirmSelectedColor() {
         this._updateSelectedColor();
         this.toggle();
