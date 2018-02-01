3d41773144ff4c14128e9c210248f6ba7b965407
Tiago Temporin <tiaguinhocab@gmail.com>
2018-02-01 23:38:03 +0100

    feat(functions) add new functions and constant

---
+++ lib/src/color-picker.ts
@@ -0,0 +1,50 @@
+/** Contant used as empty color */
+export const EMPTY_COLOR = 'none';
+
+/**
+ * Verify if color has # as a first char. If not, add this char
+ * to the color
+ * @param color string
+ */
+export function coerceHexaColor(color: string): string {
+    if (!isValidColor(color)) {
+        return EMPTY_COLOR;
+    }
+
+    if (color.indexOf('#') !== 0) {
+        color = `#${color}`;
+    }
+
+    return color.toUpperCase();
+}
+
+/**
+ * Validate if the color is valid
+ * @param color string
+ */
+export function isValidColor(color: string): boolean {
+    // validate if color is an hexadecimal
+    if (
+        !color ||
+        color.length < 6 ||
+        color.length > 7
+    ) {
+        return false;
+    }
+
+    // validate rgb of the color
+    const hex = color.replace('#', '');
+    const r = parseInt(hex.slice(0, 2), 16);
+    const g = parseInt(hex.slice(2, 4), 16);
+    const b = parseInt(hex.slice(4, 6), 16);
+
+    if (
+        Number.isNaN(r) ||
+        Number.isNaN(g) ||
+        Number.isNaN(b)
+    ) {
+        return false;
+    }
+
+    return true;
+}
