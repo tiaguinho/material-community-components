b5e742101a53a94d55dd45ee49587fab5b59ee8d
Tiago Temporin <tiaguinhocab@gmail.com>
2018-04-21 17:38:50 +0100

    feat: add prefix to router

--- src/demo-app/app/app.component.ts
+++ src/demo-app/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
   preserveWhitespaces: false,
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  prefix: string;
+
+  ngOnInit() {
+    this.prefix = environment.prefix;
+  }
+}
--- src/demo-app/app/app.router.ts
+++ src/demo-app/app/app.router.ts
@@ -1,8 +1,18 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 
 export const routes: Routes = [
-  { path: 'color-picker', loadChildren: './color-picker/color-picker.module#ColorPickerModule' },
-  { path: 'timer-picker', loadChildren: './timer-picker/timer-picker.module#TimerPickerModule' },
-  { path: 'scrollspy', loadChildren: './scrollspy/scrollspy.module#ScrollspyModule' },
+  {
+    path: `${environment.prefix}color-picker`,
+    loadChildren: './color-picker/color-picker.module#ColorPickerModule',
+  },
+  {
+    path: `${environment.prefix}timer-picker`,
+    loadChildren: './timer-picker/timer-picker.module#TimerPickerModule',
+  },
+  {
+    path: `${environment.prefix}scrollspy`,
+    loadChildren: './scrollspy/scrollspy.module#ScrollspyModule',
+  },
 ];
