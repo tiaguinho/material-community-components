cd49648dcb026a982b0749421fac32b946a866d2
Tiago Temporin <tiaguinhocab@gmail.com>
2019-07-10 20:32:06 +0100

    refactor(demo): add browser animations module

--- src/demo-app/app/app.module.ts
+++ src/demo-app/app/app.module.ts
@@ -1,18 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { MatButtonModule, MatIconModule, MatToolbarModule } from '@angular/material';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-
 import { routes } from './app.router';
 
 @NgModule({
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
-    NoopAnimationsModule,
+    BrowserAnimationsModule,
     MatButtonModule,
     MatIconModule,
     MatToolbarModule,
