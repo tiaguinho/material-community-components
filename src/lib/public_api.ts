2f21dfa176f5493f071383fa1e11de8b1a4d7467
Tiago Temporin <tiaguinhocab@gmail.com>
2019-07-10 20:31:19 +0100

    refactor: expose speed dial module

--- src/lib/public_api.ts
+++ src/lib/public_api.ts
@@ -1,3 +1,5 @@
 export * from './color-picker/index';
-export * from './timer-picker/index';
 export * from './scrollspy/index';
+export * from './speed-dial/index';
+export * from './timer-picker/index';
+
