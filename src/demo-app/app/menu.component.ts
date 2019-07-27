fd8d3b7508d79442bb6d301b537a62463f858275
Tiago Temporin <tiaguinhocab@gmail.com>
2019-07-27 12:06:06 +0100

    feat(demo): add menu component

---
+++ src/demo-app/app/menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss'],
+  preserveWhitespaces: false
+})
+export class MenuComponent {
+
+}
