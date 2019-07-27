ce0215997a833def0363710aed4208932b44c910
Tiago Temporin <tiaguinhocab@gmail.com>
2019-07-27 12:06:45 +0100

    refactor(demo): mobile version

--- src/demo-app/app/app.component.ts
+++ src/demo-app/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
   preserveWhitespaces: false,
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+
+  @ViewChild('sidenav', { static: true }) sidenav: MatSidenav
+
+  mobile: boolean = false;
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit() {
+    this.mobile = window.innerWidth < 576;
+
+    window.onresize = () => {
+      this.mobile = window.innerWidth < 576;
+    }
+
+    this.router.events.subscribe(e => {
+      if (
+        e.constructor.name === 'NavigationEnd' &&
+        this.sidenav.opened
+      ) {
+        this.sidenav.close();
+      }
+    });
+  }
+
+}
--- src/demo-app/app/app.module.ts
+++ src/demo-app/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatIconModule, MatToolbarModule } from '@angular/material';
+import { MatButtonModule, MatIconModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { routes } from './app.router';
+import { MenuComponent } from './menu.component';
 
 @NgModule({
   imports: [
@@ -14,9 +15,10 @@ import { routes } from './app.router';
     MatButtonModule,
     MatIconModule,
     MatToolbarModule,
+    MatSidenavModule
   ],
-  declarations: [AppComponent],
+  declarations: [AppComponent, MenuComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
