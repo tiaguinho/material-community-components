8ce3d29f76b31fa2ed75621e86e0bd0244d44d8d
Tiago Temporin <tiaguinhocab@gmail.com>
2020-07-15 16:32:38 -0300

    feat(color-picker): expose enable_alpha_selector injection token

--- src/lib/color-picker/public_api.ts
+++ src/lib/color-picker/public_api.ts
@@ -1,3 +1,3 @@
 export { MccColorPickerModule } from './color-picker.module';
 export { MccColorPickerService } from './color-picker.service';
-export { MccColorPickerItem, EMPTY_COLOR } from './color-picker';
+export { MccColorPickerItem, EMPTY_COLOR, ENABLE_ALPHA_SELECTOR } from './color-picker';
