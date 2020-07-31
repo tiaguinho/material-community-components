783b5683e96bb29109506277a35e59f4c04e4912
Markus Mohoritsch <markus.mohoritsch@gmx.at>
2020-07-31 03:19:34 +0200

    fix(color-picker): perfect hue selector

--- src/lib/color-picker/color-picker-selector.component.ts
+++ src/lib/color-picker/color-picker-selector.component.ts
@@ -579,13 +579,15 @@ export class MccColorPickerSelectorComponent
     this._hueSelectorContext.rect(0, 0, this._hueSelector.nativeElement.width, this._hueSelector.nativeElement.height);
 
     const grd1 = this._hueSelectorContext.createLinearGradient(0, 0, 0, this._bc.nativeElement.height);
-    grd1.addColorStop(0, 'rgba(255, 0, 0, 1)');
-    grd1.addColorStop(0.17, 'rgba(255, 255, 0, 1)');
-    grd1.addColorStop(0.34, 'rgba(0, 255, 0, 1)');
-    grd1.addColorStop(0.51, 'rgba(0, 255, 255, 1)');
-    grd1.addColorStop(0.68, 'rgba(0, 0, 255, 1)');
-    grd1.addColorStop(0.85, 'rgba(255, 0, 255, 1)');
-    grd1.addColorStop(1, 'rgba(255, 0, 0, 1)');
+    grd1.addColorStop(0, 'hsl(0, 100%, 50%)');
+    grd1.addColorStop(0.125, 'hsl(45, 100%, 50%)');
+    grd1.addColorStop(0.25, 'hsl(90, 100%, 50%)');
+    grd1.addColorStop(0.375, 'hsl(135, 100%, 50%)');
+    grd1.addColorStop(0.5, 'hsl(180, 100%, 50%)');
+    grd1.addColorStop(0.625, 'hsl(225, 100%, 50%)');
+    grd1.addColorStop(0.75, 'hsl(270, 100%, 50%)');
+    grd1.addColorStop(0.875, 'hsl(315, 100%, 50%)');
+    grd1.addColorStop(1, 'hsl(360, 100%, 50%)');
     this._hueSelectorContext.fillStyle = grd1;
     this._hueSelectorContext.fill();
   }
